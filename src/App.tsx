5594996bdc8f1013852646ff81653186c626b590
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-04-23 21:48:22 -0300

    adding hover animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,13 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
-
-const tabs = [
-  { id: 1, text: "Dashboard" },
-  { id: 2, text: "Info" },
-  { id: 3, text: "Settings" },
-];
+import { Click } from "./Click";
+import { Hover } from "./Hover";
 
 function App() {
-  const [activeTab, setActiveTab] = useState<number>(tabs[0].id);
-
   return (
-    <main className="h-screen flex items-center justify-center bg-gray-950">
-      <nav className="space-x-2">
-        {tabs.map((tab) => (
-          <button
-            onClick={() => setActiveTab(tab.id)}
-            key={tab.id}
-            type="button"
-            className="transition relative rounded-full px-2 py-1 text-sm text-white"
-          >
-            {activeTab === tab.id && (
-              <motion.span
-                layoutId="pill"
-                className="absolute inset-0 z-10 bg-white mix-blend-exclusion"
-                style={{ borderRadius: 9999 }}
-                transition={{ type: "spring", duration: 0.6 }}
-              ></motion.span>
-            )}
-            {tab.text}
-          </button>
-        ))}
-      </nav>
+    <main className="space-y-4 h-screen flex flex-col items-center justify-center bg-gray-950">
+      <p className="text-white">Click</p>
+      <Click />
+      <p className="text-white">Hover</p>
+      <Hover />
     </main>
   );
 }
