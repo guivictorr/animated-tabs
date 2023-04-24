b1630df93fc9fac5edd7cc83eab2d41f888e4028
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-04-23 22:36:33 -0300

    improving the hover example

--- src/Hover.tsx
+++ src/Hover.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const tabs = [
   { id: 1, text: "Dashboard" },
@@ -8,22 +8,40 @@ const tabs = [
 ];
 
 export function Hover() {
-  const [activeTab, setActiveTab] = useState<number>(tabs[0].id);
+  const [hoveredTab, setHoveredTab] = useState<number | null>(null);
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
 
   return (
     <nav className="space-x-2">
       {tabs.map((tab) => (
         <button
-          onMouseOver={() => setActiveTab(tab.id)}
+          onClick={() => setActiveTab(tab.id)}
+          onMouseOver={() => setHoveredTab(tab.id)}
+          onMouseOut={() => setHoveredTab(null)}
           key={tab.id}
           type="button"
           className="transition relative rounded-sm px-2 py-1 text-sm text-white"
         >
+          <AnimatePresence>
+            {hoveredTab === tab.id && (
+              <motion.span
+                layoutId="hover-pill"
+                className="absolute inset-0 z-10 bg-white/20"
+                style={{
+                  borderRadius: 4,
+                }}
+                transition={{ type: "spring", duration: 0.6 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              ></motion.span>
+            )}
+          </AnimatePresence>
           {activeTab === tab.id && (
             <motion.span
-              layoutId="hover-pill"
-              className="absolute inset-0 z-10 bg-white mix-blend-exclusion"
-              style={{ borderRadius: 4 }}
+              layoutId="active-hover-tab"
+              className="absolute -bottom-2 left-0 w-full z-10 bg-white h-[2px]"
+              style={{ borderRadius: 9999 }}
               transition={{ type: "spring", duration: 0.6 }}
             ></motion.span>
           )}
